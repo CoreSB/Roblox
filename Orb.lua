4045da9c8c9f7561e3680e01045ed08b13fadff7
RobloxSbScripter <ISwetHD@gmail.com>
2015-08-23 14:20:35 +0300

    Update Orb.lua

--- Orb.lua
+++ Orb.lua
@@ -1,1 +1,5 @@
+--[[Scripted by] RobloxSbScripter]]
 
+
+local player = game:GetService("Players").LocalPlayer or owner or game:GetService("Players"):WaitForChild("Player")
+player.Character:WaitForChild("Humanoid")
